1cdf11b59b96d42c20125ced3990ca77013bd71c
Dmitrii Rykov <larkin93@icloud.com>
2024-07-05 21:12:37 +0200

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+void Main()
+
+{
+    int m = 2;
+    int n = 3;
+
+    int result = Ackermann(m, n);
+    Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+}
+
+int Ackermann(int m, int n)
+
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Ackermann(m - 1, 1);
+    else
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Main();
